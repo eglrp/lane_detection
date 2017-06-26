33e7b95f53532dbf84e57f150ac19d0e5dee06c5
i am a cat <296434332@qq.com>
2017-06-26 09:32:00 +0800

    所执行主程序的框架

---
+++ run.py
@@ -0,0 +1,19 @@
+import numpy as np
+import math
+import cv2
+import time
+import matplotlib.pyplot as plt
+# 对图片进行切块，并分类，返回mat矩阵和仅保留车道线图像块的图片
+def classify(filename):
+	pass
+# 使用霍夫变换对MAT矩阵进行处理，得到直方图和每个角度穿过的图像块信息
+def houghtransport(filename):
+	pass
+# 获取第一条车道线的信息，拟合直线
+def getfirstline(angle,reclist):
+	pass
+# 获取第二条车道线信息
+def getsecondline(angle,reclist):
+	pass
+if __name__ == '__main__':
+	pass
