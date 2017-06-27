c4e8b46805e406f99d0c4eca67305103f6230af8
panda404 <296434332@qq.com>
2017-06-27 09:00:58 +0800

    Update shiyan.py

--- shiyan.py
+++ shiyan.py
@@ -1,8 +1,15 @@
 l = [1,2,3,5,6,11,23,24,25,26]
+result = []
 for i in range(len(l)):
 	l[i] = l[i]*2
 i = 0
 while i < len(l):
-	ele = [l[i]]
-	i = i + 1
-	while 
+	ele = []
+	step = 0
+	while (l[i]+step*2) in l:
+		ele.append(l[i+step])
+		step = step + 1
+	# 这里把ele插入返回的列表中
+	result.append(ele)
+	i = i + step
+print(result)
